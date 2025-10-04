b95529ef567f8d72ab1db1a5b81979e85d074b45
Luidi Augusto <luidi.augusto1234@gmail.com>
2025-10-04 16:37:57 -0300

    feat: function atualizar_jogo implementada

--- src/controller/games-controller.js
+++ src/controller/games-controller.js
@@ -7,7 +7,7 @@ export async function publicar_jogo(req, res) {
     const { title, plataform } = req.body;
 
     if (!title || !plataform) {
-        return res.status(404).json({ mensagem: "Necessita de title e plataform na requisição." })
+        return res.status(404).json({ mensagem: "Necessita de title e plataform na requisição." });
     };
 
     try {
@@ -19,10 +19,10 @@ export async function publicar_jogo(req, res) {
             }
         }); 
 
-        return res.status(201).json({ mensagem: "Jogo cadastrado com sucesso. ", new_game });  
+        return res.status(201).json({ mensagem: "Jogo cadastrado com sucesso. ", new_game });
 
     } catch (error) {
-        res.status(500).json({ mensagem: "Erro interno no servidor." })
+        res.status(500).json({ mensagem: "Erro interno no servidor." });
     };
 };
 
@@ -30,34 +30,68 @@ export async function listar_jogos(req, res) {
     try {
 
         const all_games = await prisma.games.findMany();
-        res.status(200).json({mensagem: "Jogos encontrados: " ,all_games})
+        res.status(200).json({mensagem: "Jogos encontrados: " ,all_games});
 
     } catch (error) {
-        res.status(500).json({ mensagem: "Erro interno no servidor." })
+        res.status(500).json({ mensagem: "Erro interno no servidor." });
     };
 };
 
 export async function listar_jogos_id(req, res) {
-    const id = parseInt(req.params.id);
-    
-    if (isNaN(id)) {
-        return res.status(400).json({ mensagem: "Necessita de id valido na requisição." })
+    const id_listar = parseInt(req.params.id);
+
+    if (isNaN(id_listar)) {
+        return res.status(400).json({ mensagem: "Necessita de id valido na requisição." });
     };
 
     try {
         const id_game = await prisma.games.findUnique({
             where: {
-                id: Number(id)
+                id: Number(id_listar)
             }
         });
 
         if (!id_game) {
-            return res.status(404).json({ mensagem: "Jogo e ID não encontrado no banco de dados." })
+            return res.status(404).json({ mensagem: "Jogo e ID não encontrado no banco de dados." });
         };
 
-        res.status(200).json({ mensagem: "Jogo encontrado: ", id_game })
+        res.status(200).json({ mensagem: "Jogo encontrado: ", id_game });
 
     } catch (error) {
-        res.status(500).json({ mensagem: "Erro interno no servidor." })
+        res.status(500).json({ mensagem: "Erro interno no servidor." });
+    };
+};
+
+export async function atualizar_jogo(req, res) {
+    const id_update = parseInt(req.params.id);
+
+    if (isNaN(id_update)) {
+        return res.status(400).json({ mensagem: "Necessita de id valido na requisição." });
+    };
+
+    try {
+
+        const { title, plataform, available } = req.body;
+
+        if (!title || !plataform || available === undefined) {
+            return res.status(404).json({ mensagem: "Necessita de title, plataform e available na requisição." });
+        };
+
+        const id_game_update = await prisma.games.update({
+            where: {
+                id:Number(id_update)
+            },
+
+            data: {
+                title: title,
+                plataform: plataform,
+                available: available
+            }
+        });
+
+        res.status(200).json({ mensagem: "Jogo atualizado: ", id_game_update });
+
+    } catch (error) {
+        res.status(500).json({ mensagem: "Erro interno no servidor." });
     };
 };
